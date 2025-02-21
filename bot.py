ebdb805dfa57a0d7b2b13e0cd40bd6067a1ab3ff
Aleksey <rav.92@list.ru>
2025-02-21 12:27:37 +0300

    set description

--- bot.py
+++ bot.py
@@ -18,12 +18,21 @@ dp.include_router(main_router)
 bot = Bot(config.TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 
 
+async def prepare_bot(bot: Bot) -> Bot:
+    await bot.set_my_commands([types.BotCommand(command="start", description="Main menu")])
+    await bot.set_my_description(
+        "Привет, с помощью этого бота ты сможешь стать частью нашей команды. Нажми /start чтобы начать."
+    )
+    await bot.set_my_name("PlusTeam Bot")
+    return bot
+
+
 async def main():
     try:
         logger.info("Запуск бота")
         # logging.basicConfig(level=logging.DEBUG)  # Remove
 
-        await bot.set_my_commands([types.BotCommand(command="start", description="Main menu")])
+        await prepare_bot(bot)
         await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
     except KeyboardInterrupt:
